44b0b797a7ac40af3636a6fe233dc7272c67e1ce
yuusuket <t1210190907@gmail.com>
2021-09-03 20:21:12 +0900

    属性で設定

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,5 +1,6 @@
-{{--コンポーネントとして利用。app.blade.phpのslotの中身になる。--}}
 <x-tests.app>
-    <x-slot name="header">ヘッダー１</x-slot>
+<x-slot name="header">ヘッダー１</x-slot>
 コンポーネントテスト１
+
+    <x-tests.card title="タイトル" content="本文"/>
 </x-tests.app>
