231aa09356367fb47a731d2beebd644adc49b46f
yuusuket <t1210190907@gmail.com>
2022-05-26 18:02:23 +0900

    sec04_multiLogin

--- routes/admin.php
+++ routes/admin.php
@@ -23,11 +23,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.dashboard');
 })->middleware(['auth:admin'])->name('dashboard');
 
 
