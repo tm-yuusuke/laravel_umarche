0c36c9d770ad8e13f446aeac5c0a2cb9a147a30f
yuusuket <t1210190907@gmail.com>
2022-05-31 10:24:26 +0900

    edit, update

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Owner; //Eloquent エロクアント
 use Illuminate\Support\Facades\DB; //QueryBuilder クエリビルダー
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+
 
 class OwnersController extends Controller
 {
@@ -38,7 +40,7 @@ class OwnersController extends Controller
         //var_dump($q_first);
 
         //dd($e_all, $q_get, $q_first, $c_test);
-        $owners = Owner::select('name','email','created_at')->get();
+        $owners = Owner::select('id','name','email','created_at')->get();
         return view('admin.owners.index',
         compact('owners'));
     }
@@ -61,7 +63,22 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //$request->name;
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:owners',
+            'password' => 'required|string|confirmed|min:8',
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with('message', 'オーナー登録を実施しました。');
     }
 
     /**
@@ -83,8 +100,11 @@ class OwnersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        //dd($owner);
+        return view('admin.owners.edit', compact('owner'));
     }
+
 
     /**
      * Update the specified resource in storage.
@@ -95,7 +115,16 @@ class OwnersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        $owner->name =$request->name;
+        $owner->email =$request->email;
+        $owner->password =Hash::make($request->password);
+        $owner->save();
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with('message', 'オーナー情報を更新しました。');
+
     }
 
     /**
