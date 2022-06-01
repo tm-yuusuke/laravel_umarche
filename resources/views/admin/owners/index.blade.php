3a35128aa1901a0f434e2cda09bf4575f859d311
yuusuket <t1210190907@gmail.com>
2022-06-01 14:48:52 +0900

    shop追加

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -14,7 +14,7 @@
         <div class="container md:px-5 mx-auto">
         <x-flash-message status="session('status')" />
         <div class="flex justify-end mb-4">
-          <button onclick="location.href='{{route('admin.owners.create')}}'" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>
+        <button onclick="location.href='{{route('admin.owners.create')}}'" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>
         </div>
         <div class="lg:w-2/3 w-full mx-auto overflow-auto">
             <table class="table-auto w-full text-left whitespace-no-wrap">
