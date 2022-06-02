41dc0174be696f94bc356e05469ce38670df13b3
yuusuket <t1210190907@gmail.com>
2022-06-02 11:19:47 +0900

    middleware, abort(404)

---
+++ app/Http/Controllers/Owner/ShopController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Owner;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Shop;
+
+//use function PHPUnit\Framework\isNull;
+
+class ShopController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next){
+            //dd($request->route()->parameter('shop'));//文字列
+            //dd(Auth::id());//数字
+
+            $id = $request->route()->parameter('shop');
+            if(!is_Null($id)){
+                $shopsOwnerId = Shop::findOrFail($id)->owner->id;
+                $shopId = (int)$shopsOwnerId;//cast
+                $ownerId = Auth::id();//shopとowneridが一致か確認
+                if($shopId !== $ownerId){
+                    abort(404);
+                }
+            }
+            return $next($request);
+        });
+    }
+
+    public function index()
+    {
+        $ownerId = Auth::id();
+        $shops = Shop::where('owner_id', $ownerId)->get();
+
+        return view('owner.shops.index',
+        compact('shops'));
+    }
+
+    public function edit($id)
+    {
+        dd(Shop::findOrFail($id));
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+}
