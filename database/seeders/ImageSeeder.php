1530b1e378a02d85ddc1e60709776e032aafce6a
yuusuket <t1210190907@gmail.com>
2022-06-06 15:03:55 +0900

    image seed

---
+++ database/seeders/ImageSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class ImageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('images')->insert([
+            [
+                'owner_id' => 1,
+                'filename' => 'sample1.jpg',
+                'title' => null
+            ],
+            [
+                'owner_id' => 1,
+                'filename' => 'sample2.jpg',
+                'title' => null
+            ]
+            ]);
+    }
+}
