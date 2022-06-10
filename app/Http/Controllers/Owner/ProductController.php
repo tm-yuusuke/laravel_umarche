e40d7371a1f7a7ccf19ec1381d762e3d3cf97a47
yuusuket <t1210190907@gmail.com>
2022-06-10 13:58:45 +0900

    product update_105

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -159,6 +159,45 @@ class ProductController extends Controller
             ->with(['message' => '在庫数が変更されています。再度確認してください。',
             'status' =>'alert']);
         }else{
+            try{
+                DB::transaction(function () use($request, $product) {
+
+                        $product->name = $request->name;
+                        $product->information = $request->information;
+                        $product->price = $request->price;
+                        $product->sort_order = $request->sort_order;
+                        $product->shop_id = $request->shop_id;
+                        $product->secondary_category_id = $request->category;
+                        $product->image1 = $request->image1;
+                        $product->image2 = $request->image2;
+                        $product->image3 = $request->image3;
+                        $product->image4 = $request->image4;
+                        $product->is_selling = $request->is_selling;
+
+                        $product->save();
+
+                    if($request->type === '1'){
+                        $newQuantity = $request->quantity;
+                    }
+                    if($request->type === '2'){
+                        $newQuantity = $request->quantity * -1;
+                    }
+                    Stock::create([
+                        'product_id' => $product->id,
+                        'type' => $request->type,
+                        'quantity' => $newQuantity
+                    ]);
+                }, 2);
+            }catch(Throwable $e){
+                Log::error($e);
+                throw $e;
+            }
+
+
+            return redirect()
+            ->route('owner.products.index')
+            ->with(['message' => '商品情報を更新しました。',
+            'status' =>'info']);
 
         }
         }
